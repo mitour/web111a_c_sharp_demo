e4f515225f0c116b263ff2f84b4b4d0d738c59a5
Mitour <love2082020@gmail.com>
2022-04-07 13:32:04 +0800

    course practice: console

---
+++ ConsoleEnum/ConsoleEnum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleEnum
+{
+    class Program
+    {
+        enum WeekDays : int
+        {
+            Monday = 1,
+            Tuesday = 2,
+            Wednesday = 3,
+            Thurday = 4,
+            Friday = 5,
+            Saturday = 6,
+            Sunday = 7
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"星期三咧舉常數值：{(int)WeekDays.Wednesday}");
+            Console.WriteLine($"星期五咧舉常數值：{(int)WeekDays.Friday}");
+
+        }
+    }
+}
---
+++ ConsoleShift/ConsoleShift/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleShift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            n = n << 1;
+            Console.WriteLine($"n=n<<1 左移一位：{n}");
+
+            n = n << 2;
+            Console.WriteLine($"n=n<<2 左移兩位：{n}");
+
+            n = n >> 2;
+            Console.WriteLine($"n=n>>2 右移兩位：{n}");
+
+            n = n >> 1;
+            Console.WriteLine($"n=n>>1 右移一位：{n}\n");
+
+            char c1 = '9';
+            char c2 = '2';
+            char c3 = '1';
+            Console.WriteLine($"原編碼訊息：{c1}{c2}{c3}");
+            int key = 11;
+
+            c1 = (char)(c1 ^ key);
+            c2 = (char)(c2 ^ key);
+            c3 = (char)(c3 ^ key);
+            Console.WriteLine($"編碼後訊息：{c1}{c2}{c3}");
+
+            c1 = (char)(c1 ^ key);
+            c2 = (char)(c2 ^ key);
+            c3 = (char)(c3 ^ key);
+            Console.WriteLine($"解碼後訊息：{c1}{c2}{c3}");
+        }
+    }
+}
