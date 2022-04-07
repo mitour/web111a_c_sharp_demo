9ee402c0a06e3cff48df7cfe309c13e80e734d58
Mitour <love2082020@gmail.com>
2022-04-07 09:43:05 +0800

    escape sequence practice

---
+++ ConsoleEscSeq/ConsoleEscSeq/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleEscSeq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("12345678901234567890123456789012345678901234567890");
+            Console.Write("\tEveryone say：\"Hello World\"\tWonderful\n\nWelcome To VS 2019\n檔名： c:\\cs\\hw1.cs\n檔名： c:\\cs\\hw1.cs\nC# 2019 is Cool！\n\"C# 2019 is Cool！\"\n\u0041pple電腦\n\\a\n\nBegin:");
+            Console.Read();
+        }
+    }
+}
