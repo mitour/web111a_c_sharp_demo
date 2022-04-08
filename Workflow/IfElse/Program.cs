fd660ae1422da46289afafdce29205cedcedb063
Mitour <love2082020@gmail.com>
2022-04-08 11:47:10 +0800

    0407 course execise: if/else and nested if/else

---
+++ Workflow/IfElse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 18;
+
+            if (age >= 20)
+            {
+                Console.WriteLine("我是成年人！");
+            }
+
+            if(age >= 10 && age <= 19)
+            {
+                Console.WriteLine($"我是青少年，年紀是{age}");
+            }
+            else
+            {
+                Console.WriteLine($"我不是青少年，年紀是{age}");
+            }
+        }
+    }
+}
---
+++ Workflow/NestedIfElse/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace NestedIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3;
+
+            Console.Write("請輸入第一個數字：");
+            n1 = int.Parse(Console.ReadLine());
+            Console.Write("請輸入第二個數字：");
+            n2 = int.Parse(Console.ReadLine());
+            Console.Write("請輸入第三個數字：");
+            n3 = int.Parse(Console.ReadLine());
+
+            if(n1 > n2)
+            {
+                if(n1 > n3)
+                {
+                    Console.WriteLine($"最大值是：{n1}");
+                }
+                else
+                {
+                    Console.WriteLine($"最大值是：{n3}");
+                }
+            }
+            else
+            {
+                if (n2 > n3)
+                {
+                    Console.WriteLine($"最大值是：{n2}");
+                }
+                else
+                {
+                    Console.WriteLine($"最大值是：{n3}");
+                }
+            }
+        }
+    }
+}
