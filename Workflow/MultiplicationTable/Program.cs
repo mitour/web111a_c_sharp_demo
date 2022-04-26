ad1b760931c05dad96ec1b3071b4601a7d01b65d
Mitour <love2082020@gmail.com>
2022-04-26 20:15:19 +0800

    9x9 multiplication table

---
+++ Workflow/MultiplicationTable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    // {i*j, 2} 代表format結果在兩位數
+                    Console.Write($"{j}*{i} = {i*j, 2}  ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
