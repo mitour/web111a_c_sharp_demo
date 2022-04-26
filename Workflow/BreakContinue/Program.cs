0905a4a4b47bf9c9e05651039e60b8551407481c
Mitour <love2082020@gmail.com>
2022-04-26 20:14:31 +0800

    break, continue and do/while execise

---
+++ Workflow/BreakContinue/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BreakContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int upper, N = 0, sum = 0;
+            Console.WriteLine("== 求1+2+3+...+N <= Upper（臨界值）");
+            Console.Write("\n1.請輸入臨界值：");
+            upper = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= upper; i++)
+            {
+                sum += i;
+                if (sum > upper)
+                {
+                    sum -= i;
+                    N = i-1;
+                    break;
+                }
+            }
+            Console.WriteLine($"\n2.電腦自動算出N為：{N}");
+            Console.WriteLine($"\n3.1+2+3+...+{N}={sum}");
+
+        }
+    }
+}
---
+++ Workflow/DoWhile/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0, score, average = 0;
+            string flag;
+            Console.WriteLine("== 程式語言設計成績計算 ==");
+
+            do
+            {
+                count++;
+                Console.Write($"\n{count}.請輸入第{count}位同學成績：");
+                score = int.Parse(Console.ReadLine());
+
+                average += score;
+                
+                Console.Write("\n== continue? (y/n) ");
+                flag = Console.ReadLine();
+            } while (flag == "y" || flag == "Y");
+
+            average = average / count;
+            Console.WriteLine($"\n== 共輸入{count}位同學成績，班上平均成績：{average}");
+
+        }
+    }
+}
