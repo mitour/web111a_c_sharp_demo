d4d8422279cc3f0cb4f75d389544b1d363d12c7a
Mitour <love2082020@gmail.com>
2022-04-29 11:44:34 +0800

    fibonacci sequence

---
+++ Recursion/Fibonacci/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int n;
+
+            Console.Write("Enter a number: ");
+            input = Console.ReadLine();
+
+            int.TryParse(input, out n);
+            for(int i = 1; i <= n; i++)
+            {
+                Console.Write(fib(i)+"\t");
+
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine("");
+                }
+            }
+
+            static int fib(int n)
+            {
+                if(n==1 || n == 2)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return fib(n - 1) + fib(n - 2);
+                }
+            }
+        }
+    }
+}
