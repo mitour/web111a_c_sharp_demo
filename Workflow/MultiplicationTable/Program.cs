5548a82f6f6e6ca0490abd65beff6250cb883860
Mitour <love2082020@gmail.com>
2022-04-29 11:43:05 +0800

    format multiplication table

--- Workflow/MultiplicationTable/Program.cs
+++ Workflow/MultiplicationTable/Program.cs
@@ -15,6 +15,23 @@ namespace MultiplicationTable
                 }
                 Console.WriteLine();
             }
+
+            Console.WriteLine($"=====每三個一組=====");
+
+            // 九九乘法 每三個一組
+            for(int k = 1; k <= 9; k+=3)
+            {
+                for(int i = 1; i <= 9; i++)
+                {
+                    for(int j = k; j<=(k+2); j++)
+                    {
+                        Console.Write($"{j}*{i} = {i*j, 2}\t");
+                    }
+                    Console.WriteLine("");
+
+                }
+                Console.WriteLine("");
+            }
         }
     }
 }
